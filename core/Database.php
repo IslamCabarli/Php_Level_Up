7b0c8e2c95f98d2313b302d80912d3666c0aa501
IslamCabarli <icabarli@std.beu.edu.az>
2026-08-22 03:31:14 +0400

    feat: create pdo connection with database

--- core/Database.php
+++ core/Database.php
@@ -1,1 +1,18 @@
 <?php
+class Database
+{
+    private $pdo;
+    private $host = 'localhost';
+    private $dbname = 'php_review';
+    private $username = 'root';
+    private $password = '1234';
+    public function __construct()
+    {
+        $this->pdo = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+    }
+
+    public function getPDO()
+    {
+        return $this->pdo;
+    }
+}

9c684fbdedba39313c5da4db0b6ca64d535c044d
IslamCabarli <icabarli@std.beu.edu.az>
2026-08-22 01:00:52 +0400

    feat: create demo taskcontroller and call in public/index

--- app/Controller/TaskController.php
+++ app/Controller/TaskController.php
@@ -1,1 +1,8 @@
 <?php
+class TaskController
+{
+    public function index()
+    {
+            echo '<h1>' .   "Task Page" .  "</h1>\n";
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once '../app/Controller/TaskController.php';
+
+$task = new TaskController();
+$task->index();

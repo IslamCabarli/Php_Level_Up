090285db7b2d2a92650f343818b62324eb49cd01
IslamCabarli <icabarli@std.beu.edu.az>
2026-08-22 02:18:26 +0400

    feat: create a logic between .htacces and index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$uri=  str_replace("/PHP_Review/Public", '', $uri);
 require_once '../core/Router.php';
 $router = new Router();
 $router->handle($uri);
 
+var_dump($uri);

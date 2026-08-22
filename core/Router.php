6645efe929749ee1fe9426e032e5ed39b8d8fdc1
IslamCabarli <icabarli@std.beu.edu.az>
2026-08-22 23:02:25 +0400

    refactor: remove dublicate controller initialization

--- core/Router.php
+++ core/Router.php
@@ -1,23 +1,24 @@
 <?php
 class Router
 {
+
     public function handle($uri, $method)
     {
+        require_once __DIR__ . '/../app/Controller/TaskController.php';
+         $task = new TaskController();
+
             if ( $uri == '/tasks' && $method == 'GET' )
             {
-                require_once __DIR__ . '/../app/Controller/TaskController.php';
-                $task = new TaskController();
+
                 return $task->index();
             }
             else if ( $uri == '/tasks/create' && $method == 'GET' ){
-                require_once __DIR__ . '/../app/Controller/TaskController.php';
-                $task = new TaskController();
+
                 return $task->create();
             }
             else if ( $uri == '/tasks' && $method == 'POST' )
             {
-                require_once __DIR__ . '/../app/Controller/TaskController.php';
-                $task = new TaskController();
+
                 return $task->store();
             }
             else if ( $uri == '/tasks/delete' && $method == 'GET' )
@@ -30,8 +31,7 @@ class Router
             else if ( $uri =='/tasks/edit' && $method == 'GET' )
             {
                 $id = $_GET['id'];
-                require_once __DIR__ . '/../app/Controller/TaskController.php';
-                $task = new TaskController();
+
                 return $task->edit($id);
             }
 
@@ -41,8 +41,6 @@ class Router
                 $id = $_POST['id'];
                 $description = $_POST['description'];
                 $title = $_POST['title'];
-                require_once __DIR__ . '/../app/Controller/TaskController.php';
-                $task = new TaskController();
                 return $task->update($id,$title,$description);
             }
 

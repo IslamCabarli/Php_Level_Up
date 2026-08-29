f9213c9d43b1c35b667e6f5aa426d9db74612194
IslamCabarli <icabarli@std.beu.edu.az>
2026-08-29 19:58:50 +0400

    refactor: centralize input validation

--- core/Validator.php
+++ core/Validator.php
@@ -1,1 +1,30 @@
 <?php
+class Validator
+{
+    public static function required(string $value, string $field): ?string
+    {
+        if ($value === "")
+        {
+            return "$field cannot be empty";
+        }
+        return null;
+    }
+
+    public static function minLength(string $value, int $min, string $field): ?string
+    {
+        if ( strlen($value) < $min )
+        {
+            return "$field cannot be less than $min characters";
+        }
+        return null;
+    }
+
+    public static function email(string $value): ?string
+    {
+        if (filter_var($value, FILTER_VALIDATE_EMAIL) === false)
+        {
+            return "Wrong email format";
+        }
+        return null;
+    }
+}

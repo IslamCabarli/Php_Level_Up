982a5b18ef00e346143bb37e81636e3abc1e194f
IslamCabarli <icabarli@std.beu.edu.az>
2026-08-30 21:36:11 +0400

    feat: add 403 forbidden error page

---
+++ app/View/Error/403.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>403 Forbidden</title>
+</head>
+<body>
+
+<h1>403 - Forbidden</h1>
+
+<p>You are not allowed to perform this action.</p>
+
+<a href="/PHP_Review/Public/tasks">Back to tasks</a>
+
+</body>
+</html>

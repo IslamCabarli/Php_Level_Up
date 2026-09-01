65ed4d126741800c087e1fec8a59595815375b5e
IslamCabarli <icabarli@std.beu.edu.az>
2026-09-01 20:59:55 +0400

    for github error

--- public/index.php
+++ public/index.php
@@ -39,5 +39,4 @@ $router->get('/auth/register', [AuthController::class, 'showRegister']);
 $router->post('/auth/register', [AuthController::class, 'register']);
 $router->post('/auth/logout', [AuthController::class, 'logout'], true);
 
-
 $router->dispatch();
